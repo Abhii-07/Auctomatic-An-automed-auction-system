feb4df3fac04cf242079b27cfc8fe4e51e6b42bf
Abhijeet Hiwale <abhijeethiwalemail@gmail.com>
2023-03-28 14:51:48 +0530

    Library folder added

--- src/com/auctomatic/Ui/Main.java
+++ src/com/auctomatic/Ui/Main.java
@@ -1,10 +1,15 @@
 package com.auctomatic.Ui;
 
 public class Main {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.print(Color.RED);
+		System.out.print(Color.CYAN_BACKGROUND_BRIGHT);
+		System.out.println("Welcome to Automated Auction System");
+		System.out.print(Color.RESET);
+		
+		
+		
 	}
 
 }
