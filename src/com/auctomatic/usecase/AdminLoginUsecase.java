94631c9916bfee70cdce79593157f097aae96782
Abhijeet Hiwale <abhijeethiwalemail@gmail.com>
2023-03-29 14:41:52 +0530

    Code update

--- src/com/auctomatic/usecase/AdminLoginUsecase.java
+++ src/com/auctomatic/usecase/AdminLoginUsecase.java
@@ -1,5 +1,23 @@
 package com.auctomatic.usecase;
 
-public class AdminLoginUsecase {
+import java.util.Scanner;
 
+import com.auctomatic.dao.AdminDao;
+import com.auctomatic.dao.AdminDaoImpl;
+import com.auctomatic.exception.CredentialException;
+
+public class AdminLoginUsecase {
+	 public static boolean adminLogin() throws CredentialException{
+	        Scanner sc =new Scanner(System.in);
+	        System.out.println("Enter Email-");
+	        String email=sc.next();
+	        System.out.println("Enter Password-");
+	        String password=sc.next();
+
+	        String result;
+	        AdminDao dao=new AdminDaoImpl();
+	        result=dao.AdminLogin(email,password);
+			System.out.println(result);
+			return true;
+	    }
 }
